0b797b79a122a09b308d9351eb1338d522362b03
Rodrigo <rvperes@users.noreply.github.com>
2016-04-26 17:12:48 -0300

    Added files via upload

---
+++ Lv10 - Dread Door.py
@@ -0,0 +1,17 @@
+#####################################
+# Exercícios do site Codecombat.com #
+# Autor: Rodrigo Vieira Peres       #
+# Data: 24/04/2016                  #
+#                                   #
+# Site: http://www.rodrigoperes.tk/ #
+#                                   #
+# Arquivo: Lv10 - Dread Door.py    #
+#####################################
+
+"""Destroy the door. Under 3 statements."""
+
+
+# Attack the door!
+# It will take many hits, so use a "while-true" loop.
+while True:
+    self.attack("Door")
